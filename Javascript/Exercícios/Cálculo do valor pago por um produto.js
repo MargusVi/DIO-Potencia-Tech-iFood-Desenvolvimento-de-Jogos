6b6d740e72c7f2a84abcf480d92b6035324a4881
Marcos <marcos.vinicius.da.silva.pro@gmail.com>
2023-11-01 18:44:48 -0300

    Exercício de cálculo do valor pago por um produto

---
+++ Javascript/Exercícios/Cálculo do valor pago por um produto.js
@@ -0,0 +1,26 @@
+/*
+    Elabore um algoritmo que calcule o que deve ser pago por um produto, considerando o preço normal de etiqueta e a escolha da condição de pagamento.
+    Utilize os códigos da tabela a seguir para efetuar o cálculo de acordo com a opção de pagamento:
+
+    - Débito à vista = 10% de desconto
+    - Dinheiro ou PIX à vista = 15% de desconto
+    - Em 2 vezes = preço normal
+    - Mais de 2 vezes = 10% de juros
+*/
+
+const preco = 10;
+const pagamento = "debito";
+
+let valor = 0;
+
+if(pagamento === "debito") {
+    valor = (preco - (preco * 10/100)).toFixed(2);
+} else if(pagamento === "dinheiroPix") {
+    valor = (preco - (preco * 15/100)).toFixed(2);
+} else if(pagamento === "2vezes") {
+    valor = preco.toFixed(2);
+} else if(pagamento === "maisParcelas") {
+    valor = (preco + (preco * 10/100)).toFixed(2);
+}
+
+console.log(`Seu produto sairá a R$${valor}.`);
