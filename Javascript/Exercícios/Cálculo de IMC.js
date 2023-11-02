b1ad37912547a188791dd3c327920fd8392bf2da
Marcos <marcos.vinicius.da.silva.pro@gmail.com>
2023-11-02 14:07:11 -0300

    Exercício atualizado com a utilização de objetos e classes

--- Javascript/Exercícios/Cálculo de IMC.js
+++ Javascript/Exercícios/Cálculo de IMC.js
@@ -7,23 +7,50 @@
     - Entre 25 e 30 = acima do peso
     - Entre 30 e 40 = obeso
     - Acima de 40 = obesidade grave
+
+    Crie uma classe para representar pessoas.
+    Pessoas têm nome, peso e altura.
+    As pessoas devem ter a capacidade de dizer o valor seu IMC.
+    Instancie uma pessoa chamada José, com 70Kg e 1,75 de altura e peça José que diga o valor de seu IMC.
 */
 
-const peso = 80;
-const altura = 1.80;
+class Pessoa {
+    constructor(nome, peso, altura) {
+        this.nome = nome;
+        this.peso = peso;
+        this.altura = altura;
+        this.IMC = Math.round((peso / (Math.pow(altura, 2))) * 100) / 100;
+    }
 
-const IMC = (peso / (Math.pow(altura, 2))).toFixed(2);
+    classificacaoIMC() {
+        console.log(`Seu IMC é de ${this.IMC}`);
+        if(this.IMC < 18.5) {
+            console.log("Você está abaixo do peso!");
+        } else if(this.IMC >= 18.5 && this.IMC < 25) {
+            console.log("Você está com o peso normal!");
+        } else if(this.IMC >= 25 && this.IMC < 30) {
+            console.log("Você está acima do peso!");
+        } else if(this.IMC >= 30 && this.IMC < 40) {
+            console.log("Você está obeso!");
+        } else if(this.IMC >= 40) {
+            console.log("Você está com obesidade grave!");
+        }
+    }
 
-console.log(`O IMC é ${IMC}.`);
-
-if(IMC < 18.5) {
-    console.log("Você está abaixo do peso!");
-} else if(IMC >= 18.5 && IMC < 25) {
-    console.log("Você está com o peso normal!");
-} else if(IMC >= 25 && IMC < 30) {
-    console.log("Você está acima do peso!");
-} else if(IMC >= 30 && IMC < 40) {
-    console.log("Você está obeso!");
-} else if(IMC >= 40) {
-    console.log("Você está com obesidade grave!");
+    testeIMC(IMC) {
+        if(IMC === this.IMC) {
+            console.log("Parabéns! Você acertou seu IMC!");;
+        } else {
+            console.log("Você errou seu IMC.");
+            this.classificacaoIMC();
+        }
+    }
 }
+
+const pessoa1 = new Pessoa("José", 70, 1.75);
+console.log(pessoa1);
+pessoa1.classificacaoIMC();
+console.log("\n");
+pessoa1.testeIMC(22.85);
+console.log("\n");
+pessoa1.testeIMC(22.86);
