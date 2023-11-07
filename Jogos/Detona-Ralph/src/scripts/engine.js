b2e5a81bf1423ef70fa56b3aa44cff0dedad4389
Marcos <marcos.vinicius.da.silva.pro@gmail.com>
2023-11-07 18:26:53 -0300

    Adicionada condição de Game Over por vidas zeradas

--- Jogos/Detona-Ralph/src/scripts/engine.js
+++ Jogos/Detona-Ralph/src/scripts/engine.js
@@ -4,12 +4,14 @@ const state = {
       enemy: document.querySelector(".enemy"),
       timeLeft: document.querySelector("#time-left"),
       score: document.querySelector("#score"),
+      lives: document.querySelector('#lives')
     },
     values: {
       gameVelocity: 1000,
       hitPosition: 0,
       result: 0,
       curretTime: 60,
+      currentLives: 3
     },
     actions: {
       timerId: setInterval(randomSquare, 1000),
@@ -49,10 +51,19 @@ const state = {
     state.view.squares.forEach((square) => {
       square.addEventListener("mousedown", () => {
         if (square.id === state.values.hitPosition) {
-          state.values.result++;
-          state.view.score.textContent = state.values.result;
-          state.values.hitPosition = null;
-          playSound("hit");
+            state.values.result++;
+            state.view.score.textContent = state.values.result;
+            state.values.hitPosition = null;
+            playSound("hit");
+        } else {
+            state.values.currentLives--;
+            state.view.lives.textContent = 'x' + state.values.currentLives;
+            state.values.hitPosition = null;
+            if(state.values.currentLives <= 0) {
+                clearInterval(state.actions.countDownTimerId);
+                clearInterval(state.actions.timerId);
+                alert("Game Over! O seu resultado foi: " + state.values.result);
+            }
         }
       });
     });
